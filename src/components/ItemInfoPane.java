0fcdabcfc41f8103595aaed9639438298fc0ca53
PKhing <apimrak@gmail.com>
2021-04-12 23:22:17 +0700

    Fix ItemInfoPane

--- src/components/ItemInfoPane.java
+++ src/components/ItemInfoPane.java
@@ -2,52 +2,49 @@ package components;
 
 import items.base.Item;
 import javafx.geometry.Insets;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import utils.DrawUtil;
 import utils.FontUtil;
 import utils.GameConfig;
 
-public class ItemInfoPane extends StackPane {
+public class ItemInfoPane extends VBox {
 	public ItemInfoPane(Item item, int y, int x) {
-		super();
+		
+		// Set anchor
 		if (x < GameConfig.getScreenWidth() / 2) {
 			AnchorPane.setLeftAnchor(this, (double) (x + 45 * GameConfig.getScale()));
 		} else {
 			AnchorPane.setLeftAnchor(this, (double) (x - 105 * GameConfig.getScale()));
 		}
-		
 		AnchorPane.setTopAnchor(this, (double) y);
-		addTexture();
-		VBox infoPane = new VBox();
-		this.getChildren().add(infoPane);
-		infoPane.setPadding(new Insets(5 * GameConfig.getScale(), 0, 0, 5 * GameConfig.getScale()));
-
+		
+		// Set style
+		this.setBackground(new Background(new BackgroundFill(Color.rgb(245, 246, 231),null,null)));
+		this.setPadding(new Insets(0, 5 * GameConfig.getScale(), 5 * GameConfig.getScale(), 5 * GameConfig.getScale()));
+		this.setBorder(new Border(
+				new BorderStroke(Color.rgb(87, 89, 66), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(GameConfig.getScale()))));
+		
+		// Add name
 		Text name = new Text(item.getName());
 		name.setFont(FontUtil.getFont(18));
 		name.setWrappingWidth(90.0 * GameConfig.getScale());
 		name.setFill(Color.rgb(76, 77, 56));
-		infoPane.getChildren().add(name);
+		this.getChildren().add(name);
 
+		// Add description
 		Text description = new Text(item.getDescription());
 		description.setWrappingWidth(90.0 * GameConfig.getScale());
 		description.setFont(FontUtil.getFont(12));
 		description.setFill(Color.rgb(94, 94, 70));
-		infoPane.getChildren().add(description);
-
-	}
-
-	public void addTexture() {
-		Canvas canvas = new Canvas(100 * GameConfig.getScale(), 50 * GameConfig.getScale());
-		PixelReader itemInfoSprite = DrawUtil.getImagePixelReader("sprites/itemInfo.png");
-		WritableImage img = new WritableImage(itemInfoSprite, 0, 0, 100, 50);
-		canvas.getGraphicsContext2D().drawImage(DrawUtil.scaleUp(img, GameConfig.getScale()), 0, 0);
-		this.getChildren().add(canvas);
+		this.getChildren().add(description);
 	}
 }
