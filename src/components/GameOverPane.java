b814f85e312042d410986be7ea926c29dbef196f
miello <partearth1212@gmail.com>
2021-04-14 20:47:02 +0700

    Make a new better(?) landing and setting pane

--- src/components/GameOverPane.java
+++ src/components/GameOverPane.java
@@ -4,6 +4,7 @@ import controller.GameController;
 import controller.SceneController;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -18,26 +19,45 @@ import utils.GameConfig;
 
 public class GameOverPane extends VBox {
 	private int heightBox = 40;
+	
 	private int widthBox = 80;
 	
+	private Text gameOverTitle;
+	
+	private HBox buttonBox;
+
 	public GameOverPane() {
+		styleSetup();
+		addGameOverTitle();
+		addButtonBox();
+		addButtontoBox();
+		
+		getChildren().addAll(gameOverTitle, buttonBox);
+	}
+	
+	private void styleSetup() {
 		setPrefHeight(heightBox * GameConfig.getScale());
 		setPrefWidth(widthBox * GameConfig.getScale());
 		setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 		setAlignment(Pos.CENTER);
 		setSpacing(20.0);
-		
-		Text gameOverTitle = new Text("Game Over");
+	}
+	
+	private void addGameOverTitle() {
+		gameOverTitle = new Text("Game Over");
 		gameOverTitle.setFont(FontUtil.getFont(30));
 		gameOverTitle.setFill(Color.WHITE);
 		gameOverTitle.setTextAlignment(TextAlignment.CENTER);
-		
-		HBox buttonBox = new HBox();
+	}
+	
+	private void addButtonBox() {
+		buttonBox = new HBox();
 		buttonBox.setSpacing(15.0 * GameConfig.getScale());
 		buttonBox.setAlignment(Pos.CENTER);
-		
+	}
+	
+	private void addButtontoBox() {
 		Button tryAgainBtn = new Button("Start New Game");
-		
 		tryAgainBtn.setOnMouseClicked((event) -> GameController.start());
 		
 		Button backToMenuBtn = new Button("Back To Main Menu");
@@ -47,7 +67,5 @@ public class GameOverPane extends VBox {
 		exitBtn.setOnMouseClicked((event) -> SceneController.exitGame());
 		
 		buttonBox.getChildren().addAll(tryAgainBtn, backToMenuBtn, exitBtn);
-		
-		getChildren().addAll(gameOverTitle, buttonBox);
 	}
 }
--- src/scene/LandingScene.java
+++ src/scene/LandingScene.java
@@ -1,6 +1,7 @@
 package scene;
 
 import components.SettingPane;
+import components.StyledButton;
 import controller.GameController;
 import controller.SceneController;
 import javafx.animation.FadeTransition;
@@ -11,23 +12,37 @@ import javafx.geometry.Pos;
 import javafx.scene.CacheHint;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
+import utils.DrawUtil;
 import utils.FontUtil;
 import utils.GameAudioUtils;
+import utils.GameConfig;
+import utils.TransitionUtil;
 
 public class LandingScene {
 	private static Scene cachedScene = null;
 	private static MediaPlayer bgm = GameAudioUtils.LandingSceneBGM;
-
+	private static int widthBox = 100;
+	private static int heightBox = 60;
+	
 	public static Scene getScene() {
 		bgm.play();
 
@@ -36,61 +51,74 @@ public class LandingScene {
 		}
 
 		StackPane root = new StackPane();
-
+		
 		root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+		
+		VBox container = new VBox();
 
-		VBox box = new VBox();
+		container.setAlignment(Pos.CENTER);
+		container.setBackground(new Background(new BackgroundImage(DrawUtil.getWritableImage("landingBG.jpg"), null, null, null, new BackgroundSize(GameConfig.getScreenWidth(), GameConfig.getScreenHeight(), false, false, false, false))));
+		VBox buttonBox = new VBox();
 
-		box.setAlignment(Pos.CENTER);
+		buttonBox.setBorder(new Border(
+				new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		buttonBox.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+		buttonBox.setSpacing(10.0);
+		buttonBox.setAlignment(Pos.CENTER);
+		buttonBox.setPadding(new Insets(15));
 
-		box.setSpacing(10.0);
+		buttonBox.setMaxHeight(heightBox * GameConfig.getScale());
+		buttonBox.setPrefHeight(heightBox * GameConfig.getScale());
 
-		box.setPadding(new Insets(25, 25, 25, 25));
+		buttonBox.setMaxWidth(widthBox * GameConfig.getScale());
+		buttonBox.setPrefWidth(widthBox * GameConfig.getScale());
 
 		// Title Text
 
 		Text titleText = new Text("Pixoul");
 		titleText.setFont(FontUtil.getFont(30));
-		titleText.setFill(Color.WHITE);
-
-		box.getChildren().add(titleText);
+		titleText.setFill(Color.LIGHTGRAY);
+		VBox.setMargin(titleText, new Insets(0, 0, 15, 0));
+		titleText.setTextAlignment(TextAlignment.CENTER);
 
 		// Fading animation setup
 
-		FadeTransition fading = new FadeTransition(Duration.seconds(1.0), box);
-		fading.setFromValue(1.0);
-		fading.setToValue(0.0);
+		FadeTransition fading = TransitionUtil.makeFadingNode(container, 1.0, 0.0);
 
-		box.setCache(true);
-		box.setCacheShape(true);
-		box.setCacheHint(CacheHint.DEFAULT);
+		buttonBox.setCache(true);
+		buttonBox.setCacheShape(true);
+		buttonBox.setCacheHint(CacheHint.DEFAULT);
 
 		// Start Button
-		Button startBtn = new Button("Start");
+		Button startBtn = new StyledButton(widthBox, "Start", Color.WHITE, Color.BLACK);
+		startBtn.setTextFill(Color.WHITE);
 		fading.setOnFinished((event) -> {
 			bgm.stop();
 			bgm.seek(Duration.ZERO);
-//			SceneController.setSceneToStage(GameOverScene.getScene());
 			GameController.start();
-			box.setOpacity(1.0);
+			container.setOpacity(1.0);
 		});
 
 		startBtn.setOnMouseClicked((event) -> fading.play());
 
 		// Exit button
-		Button optionBtn = new Button("Option");
+		Button optionBtn = new StyledButton(widthBox, "Option", Color.WHITE, Color.BLACK);
+		optionBtn.setTextFill(Color.WHITE);
 		optionBtn.setOnMouseClicked((event) -> {
 			SettingPane settingPane = new SettingPane();
 			root.getChildren().add(settingPane);
 			settingPane.requestFocus();
 		});
 
-		Button exitBtn = new Button("Exit");
+		Button exitBtn = new StyledButton(widthBox, "Exit", Color.WHITE, Color.BLACK);
+		exitBtn.setTextFill(Color.WHITE);
 		exitBtn.setOnMouseClicked((event) -> SceneController.exitGame());
 
-		box.getChildren().addAll(startBtn, optionBtn, exitBtn);
+		root.getChildren().add(container);
 
-		root.getChildren().add(box);
+		container.getChildren().addAll(titleText, buttonBox);
+
+		buttonBox.getChildren().addAll(startBtn, optionBtn, exitBtn);
 
 		Scene scene = SceneController.makeNewScene(root);
 		cachedScene = scene;
