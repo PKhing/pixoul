43a834fb1c3709d369aed37f823dbc7c1b1112c0
PKhing <apimrak@gmail.com>
2021-04-15 17:41:29 +0700

    Write documentation for MapGenerator (not finished yet)

--- src/logic/MapGenerator.java
+++ src/logic/MapGenerator.java
@@ -9,24 +9,65 @@ import javafx.util.Pair;
 import utils.GameConfig;
 import utils.RandomUtil;
 
+/**
+ * An instance of this class is used to generate {@link GameMap}.
+ * 
+ */
 public class MapGenerator {
 
+	/**
+	 * A constant holding the maximum amount of room.
+	 */
 	private static final int MAX_ROOM = 10;
+	/**
+	 * A constant holding the maximum amount of path.
+	 */
 	private static final int MAX_PATH = 25;
+	/**
+	 * A constant holding the maximum length of path.
+	 */
 	private static final int MAX_LENGTH = 15;
+	/**
+	 * A constant holding the minimum length of path.
+	 */
 	private static final int MIN_LENGTH = 4;
+	/**
+	 * Representing path type
+	 */
 	private static final int PATH = -1;
+	/**
+	 * Representing room type
+	 */
 	private static final int ROOM = -2;
+	/**
+	 * Represents processing type.
+	 */
 	private static final int PROCESSING = -3;
+	/**
+	 * Represents walking straight action.
+	 */
 	private static final int STRAIGHT = 1;
+	/**
+	 * Represents turning left action.
+	 */
 	private static final int TURN_LEFT = 2;
+	/**
+	 * Represents turning right action.
+	 */
 	private static final int TURN_RIGHT = 3;
-
+	/**
+	 * Array of integer that represents type of each cell.
+	 */
 	private static int map[][];
 
+	/**
+	 * Generates new {@link GameMap}.
+	 * 
+	 * @return Randomly generated {@link GameMap}.
+	 */
 	public static GameMap generateMap() {
 		GameMap gameMap = buildNewEmptyMap();
-
+		gameMap.printMap();
 		Pair<Integer, Integer> posLadderUp = gameMap.getRoomList().get(0);
 		Pair<Integer, Integer> posLadderDown = gameMap.getRoomList().get(gameMap.getRoomList().size() - 1);
 
@@ -39,22 +80,57 @@ public class MapGenerator {
 		return gameMap;
 	}
 
+	/**
+	 * A class to represent position and direction. This class is used for path
+	 * creating only.
+	 *
+	 */
 	static class State {
-		private int y, x, direction;
+		/**
+		 * Position in the Y-axis of this state.
+		 */
+		private int y;
+		/**
+		 * Position in the X-axis of this state.
+		 */
+		private int x;
+		/**
+		 * Direction of this state.
+		 * 
+		 * @see Direction
+		 */
+		private int direction;
 
+		/**
+		 * Creates a new state.
+		 * 
+		 * @param x         position in Y-axis of this state.
+		 * @param x         position in X-axis of this state.
+		 * @param direction direction of this state.
+		 */
 		public State(int y, int x, int direction) {
 			this.y = y;
 			this.x = x;
 			this.direction = direction;
 		}
 
-		public State newState() {
+		/**
+		 * Creates a copy of this state.
+		 * 
+		 * @return a copy of this state.
+		 */
+		public State Duplicate() {
 			return new State(y, x, direction);
 		}
 
+		/**
+		 * Checks if this state is valid to create a path or not.
+		 * 
+		 * @return true if this state is valid; false otherwise.
+		 */
 		public boolean isValid() {
 			if ((y <= 0) || (x <= 0) || (y >= GameConfig.MAP_SIZE) || (x >= GameConfig.MAP_SIZE)) {
-				return true;
+				return false;
 			}
 			if (isConnectTo(PROCESSING)) {
 				return false;
@@ -62,10 +138,17 @@ public class MapGenerator {
 			return true;
 		}
 
+		/**
+		 * Checks if cells in the front left or right have a specified type or not.
+		 * 
+		 * @param type the type to check.
+		 * @return true if this state connects to a cell with a specified type; false
+		 *         otherwise.
+		 */
 		public boolean isConnectTo(int type) {
-			State f = this.newState();
-			State l = this.newState();
-			State r = this.newState();
+			State f = this.Duplicate();
+			State l = this.Duplicate();
+			State r = this.Duplicate();
 			f.move();
 			l.turnLeft();
 			l.move();
@@ -77,6 +160,9 @@ public class MapGenerator {
 			return false;
 		}
 
+		/**
+		 * Moves this state forward one cell.
+		 */
 		public void move() {
 			if (direction == 0) {
 				y--;
@@ -89,6 +175,9 @@ public class MapGenerator {
 			}
 		}
 
+		/**
+		 * Changes the direction of this state by turning left.
+		 */
 		public void turnLeft() {
 			direction--;
 			if (direction == -1) {
@@ -96,6 +185,9 @@ public class MapGenerator {
 			}
 		}
 
+		/**
+		 * Changes the direction of this state by turning right.
+		 */
 		public void turnRight() {
 			direction++;
 			if (direction == 4) {
@@ -103,6 +195,12 @@ public class MapGenerator {
 			}
 		}
 
+		/**
+		 * Does the specified action.
+		 * 
+		 * @param action the action to be done. It can be <code>STRAIGHT</code>,
+		 *               <code>TURN_LEFT</code> or <code>TURN_RIGHT</code>.
+		 */
 		public void doAction(int action) {
 			if (action == TURN_LEFT) {
 				this.turnLeft();
@@ -113,38 +211,57 @@ public class MapGenerator {
 			this.move();
 		}
 
+		/**
+		 * Gets the type of cell that this state is on.
+		 * 
+		 * @return type of the cell
+		 */
 		public int getType() {
 			if ((y <= 0) || (x <= 0) || (y >= GameConfig.MAP_SIZE) || (x >= GameConfig.MAP_SIZE))
 				return -1000;
 			return map[y][x];
 		}
 
+		/**
+		 * Sets type of cell that this state is on.
+		 * 
+		 * @param type type to be set.
+		 */
 		public void setType(int type) {
 			map[y][x] = type;
 		}
 	}
 
+	/**
+	 * Generates new empty map.
+	 * 
+	 * @return randomly created empty map.
+	 */
 	private static GameMap buildNewEmptyMap() {
 		GameMap gameMap;
 		do {
 			gameMap = new GameMap();
 			map = new int[GameConfig.MAP_SIZE + 10][GameConfig.MAP_SIZE + 10];
+
+			// Generates rooms
 			int roomCnt = 1;
 			while (roomCnt <= MAX_ROOM) {
-				int y = RandomUtil.random(0, GameConfig.MAP_SIZE);
-				int x = RandomUtil.random(0, GameConfig.MAP_SIZE);
+				int y = RandomUtil.random(1, GameConfig.MAP_SIZE - 1);
+				int x = RandomUtil.random(1, GameConfig.MAP_SIZE - 1);
 				if (makeRoom(y, x, roomCnt)) {
 					gameMap.getRoomList().add(new Pair<>(y, x));
 					roomCnt++;
 				}
 			}
+
+			// Generates paths
 			int pathCnt = 1;
 			while (pathCnt <= MAX_PATH) {
-				int y = RandomUtil.random(0, GameConfig.MAP_SIZE);
-				int x = RandomUtil.random(0, GameConfig.MAP_SIZE);
+				int y = RandomUtil.random(1, GameConfig.MAP_SIZE - 1);
+				int x = RandomUtil.random(1, GameConfig.MAP_SIZE - 1);
 				while (map[y][x] < 1) {
-					y = RandomUtil.random(0, GameConfig.MAP_SIZE);
-					x = RandomUtil.random(0, GameConfig.MAP_SIZE);
+					y = RandomUtil.random(1, GameConfig.MAP_SIZE - 1);
+					x = RandomUtil.random(1, GameConfig.MAP_SIZE - 1);
 				}
 				State state = new State(y, x, RandomUtil.random(0, 3));
 				int tmp = state.getType();
@@ -157,7 +274,9 @@ public class MapGenerator {
 				}
 			}
 			makeMap(gameMap.getGameMap());
-		} while (!validate(gameMap));
+
+			// If the map is not valid, generates again
+		} while (!isValid(gameMap));
 
 		return gameMap;
 	}
@@ -216,7 +335,7 @@ public class MapGenerator {
 		RandomUtil.shuffle(actionType);
 
 		for (int i = 0; i < 3; i++) {
-			State newState = state.newState();
+			State newState = state.Duplicate();
 			newState.doAction(actionType[i]);
 			if (makePath(newState, startRoom, length + 1)) {
 				state.setType(PATH);
@@ -257,7 +376,7 @@ public class MapGenerator {
 		}
 	}
 
-	private static boolean validate(GameMap gameMap) {
+	private static boolean isValid(GameMap gameMap) {
 		boolean[][] visit = new boolean[GameConfig.MAP_SIZE + 10][GameConfig.MAP_SIZE + 10];
 
 		int move[][] = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
