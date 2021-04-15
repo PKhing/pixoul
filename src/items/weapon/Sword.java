58b46b9d9b42a3837ab5e234e1b99bf7dd9e57b2
miello <partearth1212@gmail.com>
2021-04-15 21:01:44 +0700

    Remove unneccessary field and rename method name in weapon

--- src/items/weapon/Sword.java
+++ src/items/weapon/Sword.java
@@ -6,22 +6,21 @@ import logic.Sprites;
 
 public class Sword extends Weapon {
 
-	public Sword(String name, String description, int atk) {
-		super(name, description, atk);
-		// TODO Auto-generated constructor stub
+	public Sword(String name, String description, int attack) {
+		super(name, description, attack);
 	}
 
 	@Override
 	public void onEquip(Player player) {
 		// TODO Auto-generated method stub
-		player.setAttack(player.getAttack() + getAtk());
+		player.setAttack(player.getAttack() + getAttack());
 		player.setEquippedWeapon(this);
 	}
 
 	@Override
 	public void onUnequip(Player player) {
 		// TODO Auto-generated method stub
-		player.setAttack(player.getAttack() - getAtk());
+		player.setAttack(player.getAttack() - getAttack());
 		player.setEquippedWeapon(null);
 	}
 
