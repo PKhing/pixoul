398049842b16f473ff93b473832ebb44b5b8ffe3
PKhing <apimrak@gmail.com>
2021-04-17 17:06:50 +0700

    Write documentation for Sprites class

--- src/logic/Sprites.java
+++ src/logic/Sprites.java
@@ -1,32 +1,105 @@
 package logic;
 
+/**
+ * The Sprites class provides a set of values for representing the sprite index.
+ *
+ */
 public class Sprites {
+
+	/* Item */
+
+	/**
+	 * Represents sword sprite index.
+	 */
 	public static final int SWORD = 0;
+	/**
+	 * Represents spear sprite index.
+	 */
 	public static final int SPEAR = 1;
+	/**
+	 * Represents knife sprite index.
+	 */
 	public static final int KNIFE = 2;
+	/**
+	 * Represents armor sprite index.
+	 */
 	public static final int ARMOR = 3;
+	/**
+	 * Represents potion sprite index.
+	 */
 	public static final int POTION = 4;
+	/**
+	 * Represents small potion sprite index.
+	 */
 	public static final int SMALL_POTION = 5;
 
-	// Armor
+	/* Armor */
+
+	/**
+	 * Represents wooden armor sprite index.
+	 */
 	public static final int WOODEN_ARMOR = 0;
+	/**
+	 * Represents iron armor sprite index.
+	 */
 	public static final int IRON_ARMOR = 1;
+	/**
+	 * Represents golden armor sprite index.
+	 */
 	public static final int GOLDEN_ARMOR = 2;
 
-	// Potion
+	/* Potion */
+
+	/**
+	 * Represents strength potion sprite index.
+	 */
 	public static final int STRENGTH_POTION = 0;
+	/**
+	 * Represents shield potion sprite index.
+	 */
 	public static final int SHIELD_POTION = 1;
+	/**
+	 * Represents healing potion sprite index.
+	 */
 	public static final int HEALING_POTION = 2;
+	/**
+	 * Represents vision potion sprite index.
+	 */
 	public static final int VISION_POTION = 3;
 
-	// Entity
+	/* Entity */
+
+	/**
+	 * Represents player sprite index.
+	 */
 	public static final int PLAYER = 0;
+	/**
+	 * Represents skeleton sprite index.
+	 */
 	public static final int SKELETON = 1;
+	/**
+	 * Represents soul sprite index.
+	 */
 	public static final int SOUL = 2;
+	/**
+	 * Represents friendly soul sprite index.
+	 */
 	public static final int FRIENDLY_SOUL = 3;
+	/**
+	 * Represents reaper sprite index.
+	 */
 	public static final int REAPER = 4;
+	/**
+	 * Represents pumpkin head sprite index.
+	 */
 	public static final int PUMPKIN_HEAD = 5;
+	/**
+	 * Represents haunted maid sprite index.
+	 */
 	public static final int HAUNTED_MAID = 6;
+	/**
+	 * Represents dark mage sprite index.
+	 */
 	public static final int DARK_MAGE = 7;
 
 }
