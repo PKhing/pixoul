30155d90ff93b82b1c5951d626b79011e2d5b81f
miello <partearth1212@gmail.com>
2021-05-18 00:10:24 +0700

    Write documentation for SceneController
    Please recheck my writing and meaning of each method

--- src/controller/SceneController.java
+++ src/controller/SceneController.java
@@ -8,9 +8,23 @@ import scene.GameScene;
 import scene.LandingScene;
 import utils.GameConfig;
 
+/**
+ * The controller class that control the scene which registered to
+ * {@link #mainStage} including bring user back to main menu or exit the program
+ *
+ */
 public class SceneController {
+
+	/**
+	 * Represent the {@link Stage} that is currently used for display in window
+	 */
 	private static Stage mainStage;
 
+	/**
+	 * Setter for {@link #mainStage}
+	 * 
+	 * @param mainStage
+	 */
 	public static void setMainStage(Stage mainStage) {
 		if (mainStage == null) {
 			return;
@@ -18,6 +32,11 @@ public class SceneController {
 		SceneController.mainStage = mainStage;
 	}
 
+	/**
+	 * This method will set new {@link Scene} to {@link #mainStage}
+	 * 
+	 * @param newScene the scene which will be set {@link #mainStage}
+	 */
 	public static void setSceneToStage(Scene newScene) {
 		if (newScene == null) {
 			return;
@@ -29,20 +48,38 @@ public class SceneController {
 		newScene.setCursor(Cursor.DEFAULT);
 	}
 
+	/**
+	 * This method will call {@link Stage#show() show()} to display current stage to
+	 * window
+	 */
 	public static void showStage() {
 		mainStage.show();
 	}
 
+	/**
+	 * Handler method that will bring back to main menu when called
+	 * 
+	 * @see {@link LandingScene}
+	 */
 	public static void backToMainMenu() {
 		GameScene.getInventoryPane().remove();
 		GameScene.getPausePane().remove();
 		setSceneToStage(LandingScene.getScene());
 	}
 
+	/**
+	 * Handler method that will exit the program when called
+	 */
 	public static void exitGame() {
 		System.exit(0);
 	}
 
+	/**
+	 * Utility method for creating new scene from {@link Parent node}
+	 * 
+	 * @param node the node that want to build new scene
+	 * @return new {@link Scene}
+	 */
 	public static Scene makeNewScene(Parent node) {
 		Scene scene = new Scene(node, GameConfig.getScreenWidth(), GameConfig.getScreenHeight());
 		return scene;
